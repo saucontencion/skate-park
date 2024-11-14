556b60e896cc512c4a57c1fe0209402f96eb503d
enoc <enoc.alejandro.castrolobo@gmail.com>
2024-11-14 02:56:47 -0300

    model de skater, tengo que ver lo de foto

--- models/skater.js
+++ models/skater.js
@@ -1,3 +1,38 @@
-class skater {
-    
-}
+const { DataTypes } = require("sequelize");
+const sequelize = require("../connection/connection");
+
+const Skater = sequelize.define('Skater', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    nombre: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    anos_experiencia: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+    },
+    especialidad: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    foto: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    estado: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+    },
+})
