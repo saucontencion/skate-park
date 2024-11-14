623923df2f3a8a6f1d1f70548d587ccc16c7f9fc
enoc <enoc.alejandro.castrolobo@gmail.com>
2024-11-14 01:19:26 -0300

    rutas

--- routes/lodemas.js
+++ routes/lodemas.js
@@ -5,5 +5,14 @@ const router = Router();
 router.get('/registro', (req,res)=>{
     res.sendFile(path.resolve(__dirname, '../views/registro.html'))
 })
+router.get('/admin', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, '../views/admin.html'))
+})
+router.get('/datos', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, '../views/datos.html'))
+})
+router.get('/login', (req,res)=>{
+    res.sendFile(path.resolve(__dirname, '../views/login.html'))
+})
 
 module.exports = router;
