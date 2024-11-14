c4cfdebca7b22fdb2d5772f2bbb5459ce3e457c6
enoc <enoc.alejandro.castrolobo@gmail.com>
2024-11-14 10:32:17 -0300

    models-connect correcta

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # skate-park
+
+string de conexion, authenticate, sync, crear la base de datos. 
+colocar los name en los input en el form. verificar la creacion y hacer un findall
+redireccionar a la vista con todos, consumiendo ese findall
--- models/skater.js
+++ models/skater.js
@@ -1,42 +1,52 @@
 const { DataTypes } = require("sequelize");
-const sequelize = require("../connection/connection");
+const sequelize = require("../connection/connection");  // Assuming you have this Sequelize connection
 
 const Skater = sequelize.define(
-    'Skater', {
+  'Skater',
+  {
     id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
     email: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     nombre: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     password: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     anos_experiencia: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     especialidad: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     foto: {
-        type: DataTypes.STRING,
-        allowNull: false,
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     estado: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
     },
-}
-)
+  }
+);
 
-module.exports = {Skater}
+// Synchronize the model with the database
+Skater.sync({ force: false })  // Use force: true to drop and recreate the table
+  .then(() => {
+    console.log("Skater table has been created or already exists.");
+  })
+  .catch((error) => {
+    console.error("Error synchronizing Skater table:", error);
+  });
+
+module.exports = { Skater };
